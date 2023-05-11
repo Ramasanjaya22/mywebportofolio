83228e833788b39d50d12b1b61043d32db757b1d
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-05-11 22:09:43 +0700

    fixing header mobile devices

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ function Header({}: Props) {
             duration: 1.5,
            }}
 
-        className='flex flex-row items-center'
+        className='flex flex-row items-center invisible sm:visible md:visible lg:visible xl:visible'
          >
         <SocialIcon target='_blank'  url="https://www.linkedin.com/in/rama-sanjaya22"
         fgColor='gray'
@@ -68,7 +68,7 @@ function Header({}: Props) {
             duration: 1.5,
            }}
            
-        className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        className='flex flex-row items-center text-gray-300 cursor-pointer visible'>
         <SocialIcon
          
                 className="cursor-pointer"
